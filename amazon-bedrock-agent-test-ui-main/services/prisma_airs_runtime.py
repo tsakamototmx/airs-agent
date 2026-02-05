3fa6677620876bdce5464efeafa890e980c6b988
tsakamototmx <115515480+tsakamototmx@users.noreply.github.com>
2026-02-05 10:00:56 +0900

    Add files via upload

---
+++ amazon-bedrock-agent-test-ui-main/services/prisma_airs_runtime.py
@@ -0,0 +1,49 @@
+import requests
+import traceback
+import json
+import logging
+import boto3
+
+ssm = boto3.client('ssm')
+logger = logging.getLogger(__name__)
+profile = "pae-sec-profile-01"
+
+
+def request_airs(prompt):
+    try:
+
+        res = ssm.get_parameter(
+            Name='/airs/key',
+            WithDecryption=True
+        )
+        res_json = json.loads(res['Parameter']['Value'])
+        
+        airs_url = res_json["url"]
+        key = res_json["key"]
+
+        json_object = {
+            "contents": [
+                {
+                    "prompt": prompt
+                }
+            ],
+            "ai_profile": {
+                "profile_name": profile
+                },
+            "metadata": {
+                    "ai_model": "Test AI model",
+                    "app_name": "pae-app-01", 
+                    "app_user": "test-user-1"
+            }
+        }
+
+        header = {'x-pan-token': key}
+
+        airs_response = requests.post(airs_url, json = json_object, headers = header)
+        json_data = json.loads(airs_response.text)
+        #recommendedAction = json_data['action']
+        #print("The recommended action for this prompt is: " + recommendedAction + ".")
+        return json_data
+    except Exception:
+        error_msg = traceback.format_exc()
+        return
